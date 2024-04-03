0f017f9e0e175f91c04f1cae834e9616703c90f8
Felix Garant <102917651+garantf@users.noreply.github.com>
2024-04-03 14:30:25 -0400

    Update creds.py

--- creds.py
+++ creds.py
@@ -1,9 +1,12 @@
 # Change these variables values with your informations
-identifiant = '<code ms>'
-motDePasse = '<mot de passe>'
+identifiant = 'CODE PERMANENT'
+motDePasse = 'NIP'
+email_receiver = 'email'
 
+#Create Twilio account to fill the next variables
+sms_sender_number ='<your Twilio number>'
+account_sid = '<Your Twilio account sid>'
+auth_token = '<Your Twilio authentification Token>'
 
-# email sender & receiver
-email_sender= '<email sender>'
-email_sender_pw= '<pw>'
-email_receiver = '<email that receives the notification>'
+email_sender= 'email sender'
+email_sender_pw= 'pw'
